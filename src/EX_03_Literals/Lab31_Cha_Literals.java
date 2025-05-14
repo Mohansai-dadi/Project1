cc94f1230b3155337fa5f49c715848bb440ed912
MohanSai-dadi <dadimonusai998@gmail.com>
2025-05-14 22:43:32 +0530

    Project4

---
+++ src/EX_03_Literals/Lab31_Cha_Literals.java
@@ -0,0 +1,49 @@
+package EX_03_Literals;
+
+public class Lab31_Cha_Literals {
+    public static void main(String[] args) {
+
+        char c1 = 'A'; // A to Z, a-z, !@#$%^&*()_+
+//        char c = "A"; // "" == String = Bunch of Char
+
+        char c2 = 'B';
+        char c3 = '@';
+        char c4 = '_';
+        char c5 = '9';
+        char c6 = '1';
+        char c7 = '(';
+        char c8 = ' '; // blank space
+
+        // Escape Char
+        char new_line = '\n';
+        char tab_line = '\t';
+        char back_space = '\b';
+        char carriage_return = '\r';
+
+        System.out.println("Mohan Sai");
+        System.out.println("Mohan" + new_line + "Sai");
+        System.out.println("Mohan\nSai");
+        System.out.println("Mohan" + tab_line + "Sai");
+        System.out.println("Mohan" + back_space + "Sai");
+        System.out.println(" ----- ");
+
+        System.out.println("Mohan is " + carriage_return + "Sai");
+
+        System.out.println("Hi, This is a First line" + new_line + "This is second line\n This is Third line");
+
+
+        char c10 = 'A';
+        //  // ASCII, (limited numbers) - A -> 65
+
+        char rupees = '₹';
+        System.out.println(rupees);
+
+        char my_laugh_smily = '\u1f69'; // :)
+        System.out.println(my_laugh_smily);
+
+        char c11 = '\u1F60';
+
+
+
+    }
+}
